561998c5480aab6e060bfb87456ea3fd8a15e76f
Georgiy Danielyan <georgiyd91@gmail.com>
2015-05-10 12:54:16 -0700

    Initial Commit

---
+++ HashTable/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HashTable
+//
+//  Created by Georgiy danielyan on 5/10/15.
+//  Copyright (c) 2015 Goga. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
