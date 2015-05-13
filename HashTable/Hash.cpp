f22b4698ea87e03bcf8144894f55cdb986e0cf99
Georgiy Danielyan <georgiyd91@gmail.com>
2015-05-13 12:31:29 -0700

    HashTable (PaulProgramming Tutorial)

---
+++ HashTable/Hash.cpp
@@ -0,0 +1,9 @@
+//
+//  hash.cpp
+//  HashTable
+//
+//  Created by Georgiy danielyan on 5/10/15.
+//  Copyright (c) 2015 Goga. All rights reserved.
+//
+
+#include "hash.h"
---
+++ HashTable/Hash.h
@@ -0,0 +1,14 @@
+//
+//  hash.h
+//  HashTable
+//
+//  Created by Georgiy danielyan on 5/10/15.
+//  Copyright (c) 2015 Goga. All rights reserved.
+//
+
+#ifndef __HashTable__hash__
+#define __HashTable__hash__
+
+#include <stdio.h>
+
+#endif /* defined(__HashTable__hash__) */
